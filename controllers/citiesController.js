0f30184379c0c17994a1036ec267dca3e4ef4d37
unknown <jua56yu@hotmail.com>
2022-08-30 12:32:29 -0500

    created foil controllers

---
+++ controllers/citiesController.js
@@ -0,0 +1,21 @@
+const  City = require('../models/City')
+
+const cityController = {
+    create: async(req,res) => {
+        const {city,country,photo,population,foundation} = req.body
+        try {
+            await new City(city,country,photo,population,foundation)
+            res.status(201).json({
+                message: 'city created',
+                sucess: true
+            })
+        } catch(error){
+            res.status(400).json({
+                message: "couldn't create event",
+                sucess: false
+            })
+        }
+    }
+}
+
+module.exports = cityController
