5283cb1b099eea2fc2795a3884a8df15620b30d8
Natalia Torres <nataliatorres88@gmail.com>
2022-08-30 21:22:29 -0500

    added more ojects to the Schema to have more city info

--- models/City.js
+++ models/City.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 
 const citiesSchema = new mongoose.Schema({
+    photo: {type:String, required:true },
     city: {type:String, required:true }, 
     country: {type:String, required:true },
-    photo: {type:String, required:true },
+    details: {type:String},
+    intro: {type:String},
     population: {type:Number, required:true}, 
     foundation: {type:Date , required:true},
     
