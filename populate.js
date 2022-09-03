ca0340fd481d2239272a77093868c939bbeea2e8
Natalia Torres <nataliatorres88@gmail.com>
2022-09-03 18:47:07 -0500

    modified populate to add users

--- populate.js
+++ populate.js
@@ -1,23 +1,43 @@
 
-require ('dotenv').config()
+require('dotenv').config()
 
 //wer import the connectio to the data base
 const db = require('./config/database')
 
 //We import the models i need to make the operations
-const City = require('./models/City')
+const User = require('./models/User')
 
+User.create(
+        {
+            name: "Cinthya",
+            lastName: " Di Risio",
+            mail: "dirisioc@mail.com",
+            password: "Kirito",
+            photo: "https://im.ge/i/Ocuw4P",
+            country: "Argentina",
+        },
+        {
+            name: "Laura",
+            lastName: "Martinez",
+            mail: "martinezl@mail.com",
+            password: "Felix",
+            photo: "https://im.ge/i/OcuNs1",
+            country: "Colombia",
 
-for(let i = 0; i < 12; i++) {
-    City.create({
-        photo: "http://localhost:4000/",
-        city: "City",
-        country: "Country",
-        details: "Details",
-        intro: "Intro",
-        population: 100000,
-        foundation: "1400"
-    })
-}
+        },
+
+        {
+
+            name: "Natalia",
+            lastName: "Torres",
+            mail: "torresn@mail.com",
+            password: "Zeus",
+            photo: "https://im.ge/i/Ocum3p",
+            country: "Holanda",
+
+        }
+
+    )
+
 
 //to call it in a new termial write: node populate.js
