003f84284662d6ed214ad1a89a66e8f32f9d93fe
Natalia Torres <nataliatorres88@gmail.com>
2022-09-03 19:49:23 -0500

    updated the comment and activity routes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
  const cityRouter = require('./cities')
  const authRouter = require('./auth')
  const itineraryRouter = require('./itineraries')
+ const activityRouter = require('./activities')
+ const commentRouter = require('./comments')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,5 +15,7 @@ router.get('/', function(req, res, next) {
 router.use('/cities', cityRouter)
 router.use('/auth', authRouter)
 router.use('/itineraries', itineraryRouter)
+router.use('/activities', activityRouter)
+router.use('/comments', commentRouter)
 
 module.exports = router;
