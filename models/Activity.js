d445efef7f3356ba21d6dfb1470a6114657277b9
Natalia Torres <nataliatorres88@gmail.com>
2022-09-05 22:48:07 -0500

    updated activity model, is not working

--- models/Activity.js
+++ models/Activity.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose')
 const activitySchema = new mongoose.Schema({
     name: {type:String, required:true },
     photo: {type:String, required:true }, 
-    itinerary: {type:mongoose.Types.ObjectId, ref:'itineraries'},
+    itinerary: {type:mongoose.Types.ObjectId, ref:'itinerary'},
     
 })
 const Activity = mongoose.model(
-    'activity',
+    'activities',
     activitySchema
 )
 
