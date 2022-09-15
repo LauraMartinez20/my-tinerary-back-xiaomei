b9025abbf5af364c2e7f7c634691ca7e4b459ad0
LauraMartinez20 <danielamartinez.1012@gmail.com>
2022-09-14 19:06:00 -0500

    added validation to city

--- models/City.js
+++ models/City.js
@@ -2,40 +2,48 @@ const mongoose = require('mongoose')
 
 const citiesSchema = new mongoose.Schema({
     photo: {
-        type:String, 
-        required:true,
+        type: String, 
+        required: true,
+        validate: function (value) {
+            if (! value.startsWith('http')) {
+                throw new Error('La URL debe comenzar con http')
+            }
+        }
     },
     city: {
         type:String, 
         required:true,
         min: 4,
-        max:500,
+        max: 100,
     }, 
     country: {
-        type:String, 
-        required:true, 
-        min: 4,
-        max:500,
-    },
+        type:String,
+         required:true,
+         min: 4,
+         max: 100,
+     },
     details: {
         type:String,
-        min: 4,
-        max:500,
+        min: 5,
+        max: 100,
     },
     intro: {
         type:String,
-        min: 4,
-        max:500,
+        min: 10,
+        max: 100,
     },
     population: {
-        type:Number, 
-        required:true
+        type:Number,
+         required:true,
+         min: 1000,
+         max: 100000000,
     }, 
-
     foundation: {
-        type:Date , 
-        required:true},
-
+        type:Date ,
+         required:true,
+         min: 4,
+         max: 4,
+    },
     
 })
 const City = mongoose.model(
