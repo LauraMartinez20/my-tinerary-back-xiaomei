c87fdfde3e6a6c152001a4fb51c806a538f19c0e
LauraMartinez20 <danielamartinez.1012@gmail.com>
2022-09-14 19:07:11 -0500

    added validation to itinerary

--- models/Itinerary.js
+++ models/Itinerary.js
@@ -1,13 +1,34 @@
 const mongoose = require('mongoose')
 
 const itinerarySchema = new mongoose.Schema({
-    name: {type:String, required:true },
+    name: {
+        type:String,
+         required:true,
+         min:4,
+         max:100
+    },
     user: {type:mongoose.Types.ObjectId, ref:'users'},
     city: {type:mongoose.Types.ObjectId, ref:'cities'}, 
-    price: {type:Number, required:true },
-    likes: {type:Array, required:true },
-    tags: {type:Array, required:true },
-    duration: {type:Number, required:true },
+    price: {
+        type:Number, 
+        required:true,
+        min:10,
+        max:500
+    },
+    likes: {
+        type:Array,
+         required:true
+    },
+    tags: {
+        type:Array,
+         required:true,
+    },
+    duration: {
+        type:Number,
+         required:true,
+         min:30,
+         max:500
+    },
     
     
 })
