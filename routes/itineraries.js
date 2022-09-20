e7f22e2d440d8542f16c721d648845ea4080b435
Natalia Torres <nataliatorres88@gmail.com>
2022-09-20 14:29:52 -0500

    added the router for  likes

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const {create, update, read, destroy, itineraries } = require('../controllers/itineraryController.js')
+const {create, update, read, destroy, itineraries, likes } = require('../controllers/itineraryController.js')
 
 router.get('/', itineraries)
 
@@ -11,6 +11,8 @@ router.patch('/:id', update);
 router.get('/:id', read);
 
 router.delete('/:id', destroy)
+
+router.patch('/like/:id', passport.authenticate('jwt', {session:false}), likes)
 
 
 module.exports = router;
