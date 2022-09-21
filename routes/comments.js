b292434badade69f53243adbef537439b4e5e209
Natalia Torres <nataliatorres88@gmail.com>
2022-09-20 19:48:01 -0500

    updated the router to include the passport

--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const {create, read } = require('../controllers/commentController.js')
+let passport = require('../config/passport')
+const {create, read, destroy, updateComment  } = require('../controllers/commentController.js')
 
 router.post('/',create);
 
+router.patch('/:id', passport.authenticate('jwt', {session:false}), updateComment )
+
 router.get('/:id', read);
+
+router.delete('/:id', destroy);
+
+
 
 module.exports = router;
