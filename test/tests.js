e08a628a17cf31a28b6575b8641161c603a1cf70
LauraMartinez20 <danielamartinez.1012@gmail.com>
2022-09-23 03:25:12 -0500

    create the test for sprint5

---
+++ test/tests.js
@@ -0,0 +1,61 @@
+const { assert } = require('chai')
+const request = require('supertest')
+const app = require('../app')
+
+describe('POST /signup', function () {
+    this.timeout(10000)
+   it('Must respond with the id', function (done) {
+        request(app)
+            .post('/auth/signup')
+            .send({
+                name:"Fenix",
+                lastName:"Fenix",
+                photo:"http://localhost:4000/images",
+                email:"fenix123@email.com",
+                password:"Fenix123",
+                role:"user", 
+                from:"google",
+            })
+            .then(response => {
+                assert.isString(response.body.id)
+                done()
+        })
+    })
+})
+
+/*describe('Patch/itinerary', function () {
+    this.timeout(20000)
+   it('The itinerary was update', function (done) {
+        request(app)
+            .patch('/itineraries/6317c7514c5cbbfeb1e8fa27')
+            .send({
+                price: 60,
+            })
+            .then(response => {
+                assert.isNumber(response.body.price)
+                done()
+            })
+    })
+    
+})*/
+
+
+
+describe('GET/itinerary from activity', function() {
+    it('itinerary found', function(done) {
+      request(app)
+        .get('/activities')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    })
+  })
+  describe('GET/city', function() {
+    it('City not found', function(done) {
+      request(app)
+        .get('/cities/all')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(400, done);
+    })
+  })
