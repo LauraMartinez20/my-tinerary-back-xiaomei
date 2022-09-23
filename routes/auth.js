6e67b1718edfb24ba77f5061ae4344cb9d1e08c3
Natalia Torres <nataliatorres88@gmail.com>
2022-09-23 10:22:11 -0500

    added profile router

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 let passport = require('../config/passport')
-const {signUp, verifyMail, signIn, signOut, verifyToken} = require('../controllers/userController.js')
+const {signUp, editProfile, verifyMail, signIn, signOut, verifyToken} = require('../controllers/userController.js')
 
 router.post('/signup',signUp);
 
@@ -10,6 +10,8 @@ router.post('/signin',signIn);
 router.get('/token', passport.authenticate('jwt', {session:false}), verifyToken)
 
 router.post('/signout',signOut);
+
+router.patch('/editprofile', passport.authenticate('jwt', {session:false}), editProfile )
 
 router.get('/verify:code',verifyMail) //property is changed but needs to be read first
 
